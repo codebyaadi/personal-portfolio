decbd38675c1da2659556e6227c3b0c21501dae0
codebyaadi <adityastar57@gmail.com>
2024-11-20 20:18:32 +0530

    chore: prettified

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,6 @@ import { ModeToggle } from './mode-toggle';
 import { DATA } from '@/constants';
 
 export default function Navbar() {
-
   const mouseX = useMotionValue(0);
   return (
     <div className='pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex h-full max-h-14 origin-bottom'>
